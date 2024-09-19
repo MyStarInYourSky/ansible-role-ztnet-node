f5d7f8faa006b67e28982203dd03f970a6771b60
starlightcrafted <dev@starlightcrafted.com>
2024-09-19 06:06:48 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,67 @@
+# Ansible role for using ZeroTier Clients with ZTNET
+
+This ansible role provides configuration of the local node and ZTNET at the same time.
+
+## Implemented
+- Joining ZeroTier Clients to networks
+- Configuration of local ZeroTier Client settings
+- Configuration of ZeroTier Client settings in ZTNET
+- Custom ZeroTier Planets
+
+## Tested Operating Systems
+
+- Ubuntu 18.04
+- Ubuntu 20.04
+- Ubuntu 22.04
+
+## Parameters
+
+### `zerotier_version`
+
+The ZeroTier version that will be installed
+
+### `zerotier_networks`
+
+Configuration of ZeroTier networks that the node will join. In the same dict, you can define node configuraitons such as `authorized: true`. Please see [here](https://ztnet.network/Rest%20Api/Personal/Network-Members/modify-a-network-member) for all possible options. Some (like tags) may not be functional yet and will return an error such as `{"fieldErrors": {"": ["Unrecognized key(s) in object: 'tags'"]}, "message": "Validation error"}}`
+
+It uses the following structure
+
+```yaml
+{{zerotier network id}}:
+  authorized: true
+```
+
+### `zerotier_localconfig`
+
+Any [local configuration (locals.conf)](https://docs.zerotier.com/config/#local-configuration-options) that needs to be set on the node.
+
+For example:
+```yaml
+zerotier_localconfig:
+  settings:
+    primaryPort: 9993
+```
+
+### `zerotier_local_api_address`
+
+If you change the ZeroTier port via `zerotier_localconfig` or a similar option, you will need to update this to the correct `host:port` pair (ex: `localhost:9994`)
+
+### `zerotier_planet`
+
+If you need a custom planet, download it from ZTNET, and then set this config after encoding the planet with base64.
+
+### `zerotier_ztnet_api_key`
+
+The ZTNET Api Key
+
+### `zerogier_ztnet_api_base_url`
+
+The ZTNET installation URL, not including `/api/v1`
+
+## Example Deployment
+
+```yaml
+zerotier_version: 1.10.6
+zerotier_networks:
+  12345: {}
+```
---
+++ library/ztnet_node_config.py
@@ -0,0 +1,177 @@
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+
+DOCUMENTATION = '''
+---
+module: ztnet_node_config
+version_added: "0.0.2"
+short_description: Manages ZTNET Node Config
+description:
+    - Manages ZTNET Node Config
+options:
+    node:
+        description:
+            - Node ID of the current Node
+        type: str
+        required: true    
+    network:
+        description:
+            - Name of the network where the Node will be configured
+        type: str
+        required: true
+    api_key:
+        description:
+            - ZTNet API Key
+        type: str
+        required: true
+    config:
+        description:
+            - Config that should be updated
+        type: dict
+        required: true
+    api_url:
+        description:
+            - URL of the API
+        type: dict
+        required: true
+author:
+- MyStarInYourSky (@mystarinyoursky)
+'''
+
+EXAMPLES = '''
+- name: Add host to ZeroTier Network
+  ztnet_node_config:
+    node: zz1234
+    network: 1234
+    api_key: 1234
+    api_url: https://somesdn.mysite.com
+    config:
+      authorized: true
+'''
+
+RETURN = r'''
+node:
+  description: ZTNet Node
+  returned: always
+  type: str
+  sample: zz12345
+network:
+  description: Network where node will be configured
+  returned: always
+  type: str
+  sample: mynetwork
+config:
+  description: New config of node
+  returned: always
+  type: dict
+  sample: {'authorised': true}
+'''
+
+import grp
+import os
+import socket
+import shutil
+import json
+import time
+import os.path
+import psutil
+
+from ansible.module_utils._text import to_bytes
+from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.urls import open_url
+import urllib.error
+
+class ZTNetNodeConfig(object):
+    """
+    ZTNet Node Config Class
+    """
+
+    def __init__(self, module):
+        self.module = module
+        self.node = module.params['node']
+        self.nwid = module.params['network']
+        self.api_url = module.params['api_url']
+        self.api_key = module.params['api_key']
+        self.target_config = module.params['config']
+
+        # Set Defaults
+        self.result = {}
+        self.result['changed'] = False
+
+    def checkNetwork(self):
+        """
+        Check if Network Exists
+        """
+        networks = self.callAPI(api_url=f'{self.api_url}/network', method="GET")
+        network_config = ([i for i in networks if i['id'] == self.nwid] or [None])[0]
+        if network_config == None:
+            self.module.fail_json(changed=False, msg=f'Unable to find network', reason="Cannot find network in list of networks we have access to")
+        else:
+            return True
+        
+    def configureNode(self):
+        members = self.callAPI(api_url=f'{self.api_url}/network/{self.nwid}/member', method="GET")
+        node_config = ([i for i in members if i['id'] == self.node] or [None])[0]
+        if node_config == None:
+            self.module.fail_json(changed=False, msg=f'Unable to find node', reason="Cannot find node in list of network members")
+        elif node_config != {**node_config, **self.target_config}:
+            node_send_config_result = self.callAPI(api_url=f'{self.api_url}/network/{self.nwid}/member/{self.node}', method="POST", data=json.dumps(self.target_config))
+            self.result['changed'] = True
+            return node_send_config_result
+        elif node_config == {**node_config, **self.target_config}:
+            return node_config
+    
+    def callAPI(self, api_url, method, data=None):
+        api_auth = {'x-ztnet-auth': self.api_key, 'Content-Type': 'application/json', 'Accept': 'application/json'}
+        try:
+            raw_resp = open_url(api_url, headers=api_auth, validate_certs=True, method=method, timeout=10, data=data, follow_redirects='all')
+            resp = json.loads(raw_resp.read())
+            return resp
+        except urllib.error.HTTPError as err:
+            if err.code == 401:
+                self.module.fail_json(changed=False, msg=f'Unable to reach ZTNET API', reason="Unauthorized, check your API Key")
+            elif err.code == 429:
+                self.module.fail_json(changed=False, msg=f'Unable to reach ZTNET API', reason="Too many Requests, slow down")
+            elif err.code == 308:
+                self.module.fail_json(changed=False, msg=f'Unable to reach ZTNET API', reason="Please ensure the Network ID and Node ID are correct")
+            else:
+                self.module.fail_json(changed=False, msg=f'Unable to reach ZTNET API', reason=json.loads(err.fp.read())['error'])  
+
+def main():
+    ssh_defaults = dict(
+        bits=0,
+        type='rsa',
+        passphrase=None,
+        comment='ansible-generated on %s' % socket.gethostname()
+    )
+
+    # Init Node Config
+    ansible_module = AnsibleModule(
+        argument_spec=dict(
+            node=dict(type='str', required=True),
+            network=dict(type='str', required=True),
+            api_key=dict(type='str', required=True),
+            config=dict(type='dict', required=True),
+            api_url=dict(type='str', required=True),
+        ),
+        supports_check_mode=True,
+    )
+    zerotier_node = ZTNetNodeConfig(ansible_module)
+
+    # Check to see if network exists
+    zerotier_node.checkNetwork()
+
+    # Apply Changes
+    result=zerotier_node.configureNode()
+
+    # Emit status
+    if zerotier_node.result['changed']:
+        ansible_module.exit_json(changed=True, msg=result)
+    else:
+        ansible_module.exit_json(changed=False, msg="ZTNet config unchanged")
+
+
+# import module snippets
+if __name__ == '__main__':
+    main()
