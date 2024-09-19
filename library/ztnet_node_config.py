6d0d56d58a5834574a540ad036b0b0d2c849bae8
starlightcrafted <dev@starlightcrafted.com>
2024-09-19 03:34:28 +0000

    fix type

--- library/ztnet_node_config.py
+++ library/ztnet_node_config.py
@@ -99,21 +99,6 @@ class ZTNetNodeConfig(object):
         self.result = {}
         self.result['changed'] = False
 
-        # Fix Config
-        self.target_config = self.fixJsonBooleans()
-
-    def fixJsonBooleans(self):
-        """
-        Fixes rendering of json.dumps where booleans become capital.
-        """
-        fixed_config = {}
-        for config, config_val in self.raw_target_config.items():
-            if isinstance(config_val, bool):
-                fixed_config[config]: repr(config_val) # type: ignore
-            else:
-                fixed_config[config]: config_val # type: ignore
-        return fixed_config
-
     def checkNetwork(self):
         """
         Check if we have access to the target network
