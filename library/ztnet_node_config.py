f3447cf7f22b167ca484143c6137eb0f17cf8549
starlightcrafted <dev@starlightcrafted.com>
2024-09-19 03:03:13 +0000

    add missing bracket for node identity

--- library/ztnet_node_config.py
+++ library/ztnet_node_config.py
@@ -128,7 +128,7 @@ class ZTNetNodeConfig(object):
         api_auth = {'x-ztnet-auth': self.api_key, 'Content-Type': 'application/json', 'Accept': 'application/json'}
         config_json = json.dumps(self.target_config)
         try:
-            raw_resp = open_url(api_url, headers=api_auth, validate_certs=True, method='POST', timeout=10, data=config_json, follow_redirects='all')
+            raw_resp = open_url(api_url, headers=api_auth, validate_certs=True, method='POST', timeout=10, data=config_json)
             self.result['changed'] = True
             resp = json.loads(raw_resp.read())
             return resp
@@ -138,7 +138,7 @@ class ZTNetNodeConfig(object):
             elif err.code == 429:
                 self.module.fail_json(changed=False, msg=f'Unable to reach ZTNET API', reason="Too many Requests, slow down")
             elif err.code == 308:
-                self.module.fail_json(changed=False, msg=f'Unable to reach ZTNET API {api_url}', reason="Please ensure the Network ID and Node ID are correct")
+                self.module.fail_json(changed=False, msg=f'Unable to reach ZTNET API {api_url}, {config_json}', reason="Please ensure the Network ID and Node ID are correct")
             else:
                 self.module.fail_json(changed=False, msg=f'Unable to reach ZTNET API', reason=err.msg)
 
