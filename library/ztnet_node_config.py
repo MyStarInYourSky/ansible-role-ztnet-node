47f9ac68c09d5f9b26744ae3512622e9d98cc7f2
starlightcrafted <dev@starlightcrafted.com>
2024-09-19 05:50:37 +0000

    add tags

--- library/ztnet_node_config.py
+++ library/ztnet_node_config.py
@@ -136,6 +136,7 @@ class ZTNetNodeConfig(object):
             elif err.code == 308:
                 self.module.fail_json(changed=False, msg=f'Unable to reach ZTNET API', reason="Please ensure the Network ID and Node ID are correct")
             else:
+                err.content_file.read()
                 self.module.fail_json(changed=False, msg=f'Unable to reach ZTNET API {data}', reason=err.msg)  
 
 def main():
