e7bde659c0a1618c7de6137f87a277f0bfe015ed
starlightcrafted <dev@starlightcrafted.com>
2024-09-21 03:55:36 +0000

    debug why change is happening

--- library/zerotier_node_config.py
+++ library/zerotier_node_config.py
@@ -149,7 +149,7 @@ class ZeroTierNodeConfig(object):
         """
         network_config = self.network_config_bundle[network]['node_config']
         configure_network = self.callAPI(api_url=f'{self.local_api_url}/network/{network}', method="POST", data=json.dumps(network_config), error_mappings={401: "Access is unauthorized.", 404: "The server cannot find the requested resource."})
-        self.result['changed'] = True
+        #self.result['changed'] = True
 
 def main():
     ssh_defaults = dict(
